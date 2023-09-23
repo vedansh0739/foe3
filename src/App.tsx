bfb16b9bca075781ee84cc35116833f4b84c2e9e
Vedansh Bagadia <vedansh0739@Vedanshs-MacBook-Air.local>
2023-09-23 09:56:15 +0530

    dine

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,14 @@ import './App.css'
 import axios from 'axios';
 function App() {
   
-    const [textData, setTextData] = useState<string | null>(null);
-  
-    useEffect(() => {
-      fetch('http://127.0.0.1:8000/polls/a')
-        .then(response => response.text())
-        .then(data => {
-          setTextData(data);
-        })
-        .catch(error => {
-          console.error('There was an error fetching the data!', error);
-        });
-    }, []);
-  
+    
 
 
   return (
     <>
       <div>
         
-      {textData} 
+     tototototo
 
       </div>
      
