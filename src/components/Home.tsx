b22399409d27d33d61dfcc7905021ac967841cc8
Vedansh Bagadia <vedansh0739@Vedanshs-MacBook-Air.local>
2023-10-24 23:54:33 +0530

    df

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import '../des.css';
 const samplePosts = [
-  { id: '1', title: 'First Blog Post', content: 'This is the content of the first post.' },
+  { id: '1', title: 't', content: 'This is the content of the first post.' },
   { id: '2', title: 'Second Blog Post', content: 'This is the content of the second post.' },
   // Add more sample posts as needed.
 ];
 
 const Home: React.FC = () => {
   return (
-    <div>
+    <div className='centered-container'>
+        <h1>Vedansh Bagadia</h1>
+
+
       {samplePosts.map(post => (
         <div key={post.id}>
           <Link to={`/post/${post.id}`}>
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-
+import '../des.css';
 interface PostParams {
   [key: string]: string | undefined;
   id: string;
 }
 
 const samplePosts = [
-  { id: '1', title: 'First Blog Post', content: 'This is the content of the first post.' },
+  { id: '1', title: 'Firsddt Blog Post', content: 'This is the content of the first post.' },
+  { id: '2', title: 'Second Blog Post', content: 'This is the content of the second post.' },
   { id: '2', title: 'Second Blog Post', content: 'This is the content of the second post.' },
   // Add more sample posts as needed.
 ];
@@ -19,7 +20,7 @@ const Post: React.FC = () => {
   if (!post) return <div>Post not found!</div>;
 
   return (
-    <div>
+    <div className='centered-container'>
       <h2>{post.title}</h2>
       <p>{post.content}</p>
     </div>
